a123b091ec937583f84422061599ff371d941c48
Gustavo Salmazo <gux.salmazo@gmail.com>
2022-01-05 16:02:16 -0300

    For-Getters-Setters-Constructors

--- src/entities/ImportProduct.java
+++ src/entities/ImportProduct.java
@@ -1,5 +1,30 @@
 package entities;
 
 public class ImportProduct extends Product {
+	
+	private Double customsFee;
 
+	public ImportProduct() {
+	}
+
+	public ImportProduct(String name, Double price, Double customsFee) {
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+
+	public Double getCustomsFee() {
+		return customsFee;
+	}
+
+	public void setCustomsFee(Double customsFee) {
+		this.customsFee = customsFee;
+	}
+	@Override
+	public String priceTag() {
+		return name + price + customsFee;
+	}
+	
+	public Double totalPrice() {
+		return price + customsFee;
+	}
 }
--- src/program/Main.java
+++ src/program/Main.java
@@ -1,5 +1,40 @@
 package program;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
 public class Main {
+	
+	public static void main(String[]args) throws ParseException {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat date1 = new SimpleDateFormat("dd/MM/yyyy");
+		List<Product> product = new ArrayList<>();
+		
+		System.out.print("Enter the number of products:");
+		int n = sc.nextInt();
+		for(int i = 1; i <= n; i++) {
+			System.out.println("Product #" + i + " data: ");
+			System.out.print("Common, used or imported? (c/u/i):");
+			char cui = sc.next().charAt(0);
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Price: ");
+			double price = sc.nextDouble();
+			if(cui == 'u') {
+				
+			}
+			
+		}
+		
+		
+	}
 
 }
